09bd5574661630a546ea7b3c5a51b321f4145fde
Robert K <robert.w.krajewski@googlemail.com>
2014-12-26 23:32:23 +0100

    Extended Logging into own tab in browser

---
+++ logger.h
@@ -0,0 +1,36 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <QWidget>
+
+namespace Ui {
+class Logger;
+}
+
+class Logger : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Logger(QWidget *parent = 0);
+    ~Logger();
+
+    void saveSettings();
+
+    void loadSettings();
+private slots:
+    void logSlot(QString message, int level);
+
+    void on_logLevelCB_currentIndexChanged(const QString &logLevel);
+
+    void on_savePB_clicked();
+
+    void on_copyPB_clicked();
+
+private:
+    QString getLogText();
+
+    Ui::Logger *ui;
+};
+
+#endif // LOGGER_H
