d672cbe204c6f7b3f8cec906883985df0d49ec3f
Stefan Ahlers <stef.ahlers@t-online.de>
2015-01-29 17:23:57 +0100

    Implements internationalisation
    This comment introduces a localisation of the project.
    Add english support (mostly ready).

---
+++ lang/sync-my-l2p_de.ts
@@ -0,0 +1,382 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE" sourcelanguage="de_DE">
+<context>
+    <name>AutoCloseDialog</name>
+    <message>
+        <location filename="../autoclosedialog.ui" line="20"/>
+        <source>Automatisch beenden</source>
+        <translation>Automatisch beenden</translation>
+    </message>
+    <message>
+        <location filename="../autoclosedialog.ui" line="36"/>
+        <source>Automatisches Beenden abbrechen</source>
+        <translation>Automatisches Beenden abbrechen</translation>
+    </message>
+</context>
+<context>
+    <name>Browser</name>
+    <message>
+        <source>Form</source>
+        <translation type="obsolete">Form</translation>
+    </message>
+    <message>
+        <location filename="../browser.ui" line="48"/>
+        <source>Daten</source>
+        <translation>Daten</translation>
+    </message>
+    <message>
+        <location filename="../browser.ui" line="57"/>
+        <source>Aktualisieren</source>
+        <translation>Aktualisieren</translation>
+    </message>
+    <message>
+        <location filename="../browser.ui" line="77"/>
+        <source>Darstellung</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../browser.ui" line="86"/>
+        <source>Neue Dateien anzeigen</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../browser.ui" line="96"/>
+        <source>Alle expandieren</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../browser.ui" line="106"/>
+        <source>Alle kontrahieren</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../browser.ui" line="116"/>
+        <source>Dateigrößenfilter</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../browser.ui" line="132"/>
+        <source> MB</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../browser.ui" line="135"/>
+        <source>maximal </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../browser.ui" line="154"/>
+        <source>Datumsfilter</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../browser.ui" line="167"/>
+        <source>&apos;ab&apos; dd.MM.yyyy</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../browser.ui" line="193"/>
+        <source>&apos;bis&apos; dd.MM.yyyy</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../browser.ui" line="219"/>
+        <source>Suche</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../browser.ui" line="234"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Gib hier den Namen (bzw. Teile davon) von Dateien/Ordnern/Veranstaltungen ein, um diese zu suchen. Gefundene Ergebnisse werden selektiert. Das Suchfenster unterstützt auch RegExp.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../browser.ui" line="240"/>
+        <source>Suchbegriff</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../browser.ui" line="250"/>
+        <source>Suchen</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../browser.ui" line="286"/>
+        <source>Synchronisation</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../browser.ui" line="295"/>
+        <source>Auswahl hinzufügen</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../browser.ui" line="305"/>
+        <source>Auswahl entfernen</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../browser.ui" line="315"/>
+        <source>Downloadordner öffnen</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../browser.ui" line="331"/>
+        <source>Download</source>
+        <translation>Download</translation>
+    </message>
+</context>
+<context>
+    <name>DateiDownloader</name>
+    <message>
+        <location filename="../dateidownloader.ui" line="98"/>
+        <source>%v/%m KBs</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../dateidownloader.ui" line="148"/>
+        <source>Download Datei 12/54</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../dateidownloader.ui" line="201"/>
+        <source>Grundgebiete der Grundgebiete</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../dateidownloader.ui" line="254"/>
+        <source>Datei.pdf</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../dateidownloader.ui" line="273"/>
+        <source>Abbrechen</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Logger</name>
+    <message>
+        <source>Form</source>
+        <translation type="obsolete">Form</translation>
+    </message>
+    <message>
+        <location filename="../logger.ui" line="60"/>
+        <source>Log-Umfang:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../logger.ui" line="67"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Standard&lt;/span&gt;: Alle wichtigen Ereignisse werden geloggt.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Erweitert&lt;/span&gt;: Neben den wichtigen Ereignissen werden auch solche geloggt, die zur Fehlerbehebung dienen.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../logger.ui" line="74"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Kopiert den gesamten Text in die Zwischenablage.&lt;/p&gt;&lt;p&gt;Um einzelne Elemente zu kopieren, müssen diese ausgewählt und mit der plattformtypischen Tastenkombination (Windows: Strg + C) kopiert werden&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../logger.ui" line="77"/>
+        <source>Kopieren</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../logger.ui" line="84"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Speichert den gesamten Text in eine separate Datei.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../logger.ui" line="87"/>
+        <source>Speichern</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>LoginDialog</name>
+    <message>
+        <location filename="../logindialog.ui" line="17"/>
+        <source>Login</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../logindialog.ui" line="30"/>
+        <source>Überprüfe Erreichbarkeit des L2P...</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>MyMainWindow</name>
+    <message>
+        <location filename="../mymainwindow.ui" line="26"/>
+        <source>Sync-my-L²P</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mymainwindow.ui" line="45"/>
+        <source>Dateibrowser</source>
+        <translation>Dateibrowser</translation>
+    </message>
+    <message>
+        <location filename="../mymainwindow.ui" line="54"/>
+        <source>Einstellungen</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mymainwindow.ui" line="66"/>
+        <source>Log</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mymainwindow.ui" line="71"/>
+        <source>Anleitung</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mymainwindow.ui" line="77"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:10pt; font-weight:600;&quot;&gt;Willkommen bei Sync-my-L²P!&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-size:10pt;&quot;&gt;In diesem Tab findet sich eine kurze Anleitung für Erstbenutzer dieses Programms.&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-size:10pt; font-weight:600;&quot;&gt;Schnellstart&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-size:10pt;&quot;&gt;Wenn du sofort loslegen willst, führe einfach folgende drei Schritte durch:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-size:10pt; text-decoration: underline;&quot;&gt;1. Einloggen&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-size:10pt;&quot;&gt;Klicke im Tab &amp;quot;Einstellungen&amp;quot; auf den Knopf auf den Knopf &amp;quot;Einloggen&amp;quot;. Kurz darauf wird sich dein Browser einloggen und du wirst aufgefordert, dich mit deiner Tim-Kennung zu authentifizieren. Danach musst du Sync-my-L²P authorisieren, damit das Programm auf deine Daten im L²P zugreifen kann. Sobald du auch dies erledigt hast, kannst du den Browser schließen und zum Programm zurückkehren.&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-size:10pt; text-decoration: underline;&quot;&gt;2. Daten aktualisieren und auswählen&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-size:10pt;&quot;&gt;Nach dem erfolgreichen Login werden automatisch die Daten aller deiner Kurse im L²P abgerufen. Dies kann einen Moment dauern. Sobald die Aktualisierung fertig ist, kannst du die abgerufenen Daten ansehen und einzelne Dateien/Verzeichnisse/Kurse/Semester von dem Download ausschließen. Auch kannst du Filter für Größe und Änderungsdatum setzen.&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-size:10pt; text-decoration: underline;&quot;&gt;3. Download&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-size:10pt;&quot;&gt;Um nun deine Dateien runterzuladen musst du noch ein Downloadverzeichnis setzen. Hierfür klickst du im Tab &amp;quot;Einstellungen&amp;quot; auf den Knopf &amp;quot;Durchsuchen&amp;quot; und wählst ein Ordner aus. Nun musst du nur noch im Tab &amp;quot;Dateibrowser&amp;quot; auf den Knopf &amp;quot;downloaden&amp;quot; drücken und Sync-my-L²P lädt alle ausgewählten Dateien für dich herrunter.&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-size:10pt; font-weight:600;&quot;&gt;Tips und Tricks&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-size:10pt;&quot;&gt;Die &lt;/span&gt;&lt;span style=&quot; font-size:10pt; text-decoration: underline;&quot;&gt;Farbe der Dateien&lt;/span&gt;&lt;span style=&quot; font-size:10pt;&quot;&gt; gibt dir Auskunft über den aktuellen Status der Dateien. Rot bedeutet &amp;quot;nicht downloaden&amp;quot;, Schwarz sind neue, nicht runtergeladene Dateien und Grün wird für bereits auf deiner Festplatte vorhandene Dateien benutzt.&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-size:10pt;&quot;&gt;Du kannst durch einen &lt;/span&gt;&lt;span style=&quot; font-size:10pt; text-decoration: underline;&quot;&gt;Doppelklick&lt;/span&gt;&lt;span style=&quot; font-size:10pt;&quot;&gt; auf Dateien diese einfach öffnen.&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-size:10pt;&quot;&gt;Im Tab &lt;/span&gt;&lt;span style=&quot; font-size:10pt; text-decoration: underline;&quot;&gt;Einstellungen&lt;/span&gt;&lt;span style=&quot; font-size:10pt;&quot;&gt; finden sich einige Optionen, mit denen du Sync-my-L²P noch komfortabler machen kannst.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Options</name>
+    <message>
+        <source>Form</source>
+        <translation type="obsolete">Form</translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="20"/>
+        <source>Login</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="26"/>
+        <source>Status: ausgeloggt</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="51"/>
+        <source>Speichern</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="64"/>
+        <source>Löschen</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="71"/>
+        <source>Einloggen</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="90"/>
+        <source>Download</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="99"/>
+        <source>Downloadverzeichnis</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="106"/>
+        <source>Durchsuchen</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="120"/>
+        <source>Filter für Rubriken</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="126"/>
+        <source>Übungen</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="136"/>
+        <source>Lernmaterialien</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="146"/>
+        <source>Medienbibliothek</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="156"/>
+        <source>Gemeinsame Dokumente</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="166"/>
+        <source>E-Mail-Anhänge</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="173"/>
+        <source>Ankündigungs-Anhänge</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="183"/>
+        <source>Automatisation</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="192"/>
+        <source>Einloggen beim Programmstart</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="205"/>
+        <source>Synchronisieren beim Programmstart</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="212"/>
+        <source>Nach der Synchronisation beenden</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="222"/>
+        <source>Sonstiges</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="231"/>
+        <source>Beim Minimieren im Tray verschwinden</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="241"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Wenn im L2P eine neue Version einer Datei vorhanden ist, wird die alte Version auf der Festplatte überschrieben.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;ACHTUNG:&lt;/span&gt; Beim Überschreiben gehen eigene Änderungen an den Dateien (Kommentare, etc.) verloren.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="244"/>
+        <source>Dateien überschreiben</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="254"/>
+        <source>Kontakt</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="260"/>
+        <source>Offizielle Webseite: &lt;html&gt;&lt;a style=&apos;text-decoration:none&apos; href=&apos;http://www.Sync-my-L2P.de&apos;&gt;http://www.Sync-my-L2P.de&lt;/a&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="276"/>
+        <source>Offizielle Facebookpage: &lt;html&gt; &lt;a style=&apos;text-decoration:none&apos; href=&apos;http://www.Facebook.de/Sync-my-L2P&apos;&gt;http://www.Facebook.de/Sync-my-L2P&lt;/a&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="289"/>
+        <source>Quellcode: &lt;html&gt; &lt;a style=&apos;text-decoration:none&apos; href=&apos;https://github.com/Sync-my-L2P/Sync-my-L2P&apos;&gt;https://github.com/Sync-my-L2P/Sync-my-L2P&lt;/a&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <QApplication>
+#include <QTranslator>
+#include <QLocale>
+
 #include "mymainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QString locale = QLocale::system().name();
+
+        QTranslator myappTranslator;
+        myappTranslator.load(":/lang/sync-my-l2p_" +locale);
+        a.installTranslator(&myappTranslator);
+
     MyMainWindow w;
     w.show();
     
