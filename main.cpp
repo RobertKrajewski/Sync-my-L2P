08ff50f8e887c36aff09499db46dd9f30b39eff7
Stefan Ahlers <stef.ahlers@t-online.de>
2015-02-09 19:40:37 +0100

    Move translation install from main.cpp to mymainwindow.cpp
    This comment improves the language switch for fallback mode. From now,
    the language switch option "Systemsprache" handles the fallback mode for
    missing translations, too.
    To support this feature, the initial translation installation is moved from
    the main.cpp into the constructor of the mymainwindow.cpp to use the same
    QTranslator "m_translator".

--- main.cpp
+++ main.cpp
@@ -1,21 +1,10 @@
 #include <QApplication>
-#include <QTranslator>
-#include <QLocale>
 
 #include "mymainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QString locale = QLocale::system().name();
-
-        // Lade lokale Sprachdatei oder falle auf Englisch zurück
-        QTranslator myappTranslator;
-        if(!myappTranslator.load(":/lang/sync-my-l2p_" +locale))
-        {
-            myappTranslator.load(":/lang/sync-my-l2p_en");
-        }
-        a.installTranslator(&myappTranslator);
 
     MyMainWindow w;
     w.show();
