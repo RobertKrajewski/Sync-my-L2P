1b404a06295fe347a1c0c220f4ec1b0d159bbcc2
Stefan Ahlers <stef.ahlers@t-online.de>
2015-02-15 13:06:48 +0100

    Rebase master

---
+++ info.cpp
@@ -0,0 +1,20 @@
+#include "info.h"
+#include "ui_info.h"
+
+
+Info::Info(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Info)
+{
+    ui->setupUi(this);
+    ui->retranslateUi(this);
+    setWindowFlags(Qt::Dialog | Qt::WindowTitleHint | Qt::CustomizeWindowHint);
+    QImage image(":/icons/Sync-my-L2P.png");
+    ui->picture->setPixmap(QPixmap::fromImage(image));
+    ui->picture->adjustSize();
+}
+
+Info::~Info()
+{
+    delete ui;
+}
