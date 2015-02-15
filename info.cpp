4dfbe1e166755eb171fd73294b9f3581532c286f
Stefan Ahlers <stef.ahlers@t-online.de>
2015-02-15 15:09:34 +0100

    Complete about widget. Add contributer tab.

--- info.cpp
+++ info.cpp
@@ -1,19 +1,18 @@
 #include "info.h"
 #include "ui_info.h"
 
-
 Info::Info(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Info)
 {
     ui->setupUi(this);
+    ui->tabWidget->setCurrentIndex(0);
     ui->retranslateUi(this);
     setWindowFlags(Qt::Dialog | Qt::WindowTitleHint | Qt::CustomizeWindowHint);
     QImage image(":/icons/Sync-my-L2P.png");
     ui->picture->setPixmap(QPixmap::fromImage(image));
     ui->picture->adjustSize();
 }
-
 Info::~Info()
 {
     delete ui;
