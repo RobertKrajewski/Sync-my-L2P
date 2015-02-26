d3fbb43aec5189c02b0a62d7758f4d68624f9e97
RobertKrajewski <robert.w.krajewski@googlemail.com>
2015-02-26 22:20:20 +0100

    Added error during compilation if client ID is missing.

--- clientId.h
+++ clientId.h
@@ -1,10 +1,14 @@
 #ifndef CLIENTID_H
 #define CLIENTID_H
 
-#endif // CLIENTID_H
 
 // Achtung: Hier muss eine gültige ClientID für den Zugriff
 //          auf die API eingetragen werden.
 //          Eine Veröffentlichung ist ID ist seitens des
 //          Rechenzentrums untersagt.
-#define CLIENTID "INSERTIDHERE"
+#define CLIENTID "INSERT YOUR CLIENT ID HERE"
+
+// Remove this line after insertion of the client ID
+#error ERROR: NO CLIENTID IN CLIENTID.H DEFINED
+
+#endif // CLIENTID_H
