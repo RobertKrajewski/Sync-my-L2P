9af5da0108c1a4f3dc4191fd5633a6fa68b8d0f3
Stefan Ahlers <stef.ahlers@t-online.de>
2015-04-04 14:51:06 +0200

    Graphical interface for e-mails and announcements
    This commit adds a GUI to show e-mails and announcements

---
+++ message.cpp
@@ -0,0 +1,14 @@
+#include "message.h"
+#include "ui_message.h"
+
+message::message(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::message)
+{
+    ui->setupUi(this);
+}
+
+message::~message()
+{
+    delete ui;
+}
