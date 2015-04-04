3446ec4fffa0a63af4a0d93465fc5176e9360d2a
Stefan Ahlers <stef.ahlers@t-online.de>
2015-04-04 14:52:02 +0200

    Complete message GUI

--- message.cpp
+++ message.cpp
@@ -6,6 +6,28 @@ message::message(QWidget *parent) :
     ui(new Ui::message)
 {
     ui->setupUi(this);
+    ui->retranslateUi(this);
+    setWindowFlags(Qt::Dialog | Qt::WindowTitleHint | Qt::CustomizeWindowHint);
+}
+
+void message::updateAuthor(QString author)
+{
+    ui->author_label->setText(author);
+}
+
+void message::updateSubject(QString subject)
+{
+    ui->subject_label->setText(subject);
+}
+
+void message::updateDate(QString date)
+{
+    ui->dates_label->setText(date);
+}
+
+void message::updateMessage(QString body)
+{
+    ui->message_body->setText(body);
 }
 
 message::~message()
