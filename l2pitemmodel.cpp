52ea130d6eb9ceea3f2a089b3da6e635b2027739
RobertKrajewski <robert.w.krajewski@googlemail.com>
2016-09-03 10:46:18 +0200

    New child items from update are only included if parent item is. Fixes #94

--- l2pitemmodel.cpp
+++ l2pitemmodel.cpp
@@ -352,20 +352,20 @@ void L2pItemModel::addFilesFromReply(QNetworkReply *reply, Structureelement *cou
             getItemList(static_cast<Structureelement*>(root->child(i)), items);
         }
 
-        QList<Structureelement*> oldItems;
         if(oldData)
         {
+            QList<Structureelement*> oldItems;
+
+            // Get old data
             root = oldData->invisibleRootItem();
-            for( int i=0; i < root->rowCount(); i++)
-            {
-                getItemList(static_cast<Structureelement*>(root->child(i)), oldItems);
-            }
+            getItemList(root, oldItems);
 
             foreach(Structureelement *item, items)
             {
-                for(QList<Structureelement*>::iterator it = oldItems.begin(); it != oldItems.end(); it++)
+                // Find an old item which fits to a new one and copy properties
+                for(auto it = oldItems.begin(); it != oldItems.end(); it++)
                 {
-                    Structureelement *oldItem = *it;
+                    auto *oldItem = *it;
                     if(item->data(urlRole) == oldItem->data(urlRole) && item->text() == oldItem->text())
                     {
                         item->setData(oldItem->data(includeRole), includeRole);
@@ -373,8 +373,17 @@ void L2pItemModel::addFilesFromReply(QNetworkReply *reply, Structureelement *cou
                         break;
                     }
                 }
+
+                // Don't include item if parent is not included
+                auto* parentItem = dynamic_cast<Structureelement*>(item->parent());
+                if(parentItem && !parentItem->data(includeRole).toBool() && item->data(includeRole).toBool())
+                {
+                    item->setData(false, includeRole);
+                }
             }
         }
+
+
 
         Utils::checkAllFilesIfSynchronised(items, options->downloadFolderLineEditText());
     }
