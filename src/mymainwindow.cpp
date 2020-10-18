ec28291a9f5020a2f0e1419f906b5fc3e809d5ca
Robert Krajewski <robert.w.krajewski@googlemail.com>
2020-10-18 16:25:28 +0200

    Change version to 2.4.2

--- src/mymainwindow.cpp
+++ src/mymainwindow.cpp
@@ -135,7 +135,7 @@ void MyMainWindow::removeOldSettings()
 
 void MyMainWindow::checkForUpdate()
 {
-    int currentVersion = 20401;
+    int currentVersion = 20402;
 
     QNetworkAccessManager manager;
     QNetworkRequest request(QUrl("https://www.syncmyl2p.de/version.txt"));
